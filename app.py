e89ff8d5f5125dfea4da53b469a5857d004632ce
Jonas Wagner <boom@confuflux.com>
2016-11-15 18:28:22 -0700

    just add the port

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -22,5 +21,4 @@ def translate():
     return render_template('translate.html', chars=chars, wordIn=wordIn)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False,host='0.0.0.0',port=port)
+    app.run(debug=False,host='0.0.0.0',port=5000)
