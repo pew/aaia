77853712b8b77dbec727fcfdb791e98c29061fb1
Jonas Wagner <boom@confuflux.com>
2018-01-28 16:39:50 -0700

    we're getting there

--- main.go
+++ main.go
@@ -6,22 +6,26 @@ import (
 	"strings"
 )
 
-var alphabet = map[string]string{"1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine", "0": "zero", "@": "at sign", " ": " ", ".": "dot", "a": "alpha", "b": "bravo", "c": "charlie", "d": "delta", "e": "echo", "f": "foxtrot", "g": "golf", "h": "hotel", "i": "india", "j": "juliet", "k": "kilo", "l": "lima", "m": "mike", "n": "november", "o": "oscar", "p": "papa", "q": "quebec", "r": "romeo", "s": "sierra", "t": "tango", "u": "uniform", "v": "victor", "w": "whiskey", "x": "xray", "y": "yankee", "z": "zulu"}
+var alphabet = map[string]string{"1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine", "0": "zero", "a": "alpha", "b": "bravo", "c": "charlie", "d": "delta", "e": "echo", "f": "foxtrot", "g": "golf", "h": "hotel", "i": "india", "j": "juliet", "k": "kilo", "l": "lima", "m": "mike", "n": "november", "o": "oscar", "p": "papa", "q": "quebec", "r": "romeo", "s": "sierra", "t": "tango", "u": "uniform", "v": "victor", "w": "whiskey", "x": "xray", "y": "yankee", "z": "zulu"}
 
 func main() {
-    args := strings.ToLower(strings.Join(os.Args[1:], ""))
+	args := strings.ToLower(strings.Join(os.Args[1:], ""))
 	input := strings.Split(args, "")
-	aaia(input)
+
+	fmt.Printf(aaia(input))
 }
 
-func aaia(in []string) {
+func aaia(in []string) string {
 	formatString := "%s - %s\n"
+	s := []string{}
 
 	for _, v := range in {
 		if v == " " {
-			fmt.Printf("\n")
-			continue
+			s = append(s, "\n")
 		}
-		fmt.Printf(formatString, v, alphabet[v])
+		if val, ok := alphabet[v]; ok {
+			s = append(s, fmt.Sprintf(formatString, v, val))
+		}
 	}
+	return strings.Join(s, "")
 }
