830027f4fc4436f0fc5ba54005c8f74abf05d23b
Jonas Wagner <boom@confuflux.com>
2018-01-28 16:09:25 -0700

    let's do this in go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var alphabet = map[string]string{"1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine", "0": "zero", "@": "at sign", " ": " ", ".": "dot", "a": "alpha", "b": "bravo", "c": "charlie", "d": "delta", "e": "echo", "f": "foxtrot", "g": "golf", "h": "hotel", "i": "india", "j": "juliet", "k": "kilo", "l": "lima", "m": "mike", "n": "november", "o": "oscar", "p": "papa", "q": "quebec", "r": "romeo", "s": "sierra", "t": "tango", "u": "uniform", "v": "victor", "w": "whiskey", "x": "xray", "y": "yankee", "z": "zulu"}
+
+func main() {
+	input := strings.Split(strings.Join(os.Args[1:], ""), "")
+	aaia(input)
+}
+
+func aaia(in []string) {
+	formatString := "%s - %s\n"
+
+	for _, v := range in {
+		if v == " " {
+			fmt.Printf("\n")
+			continue
+		}
+		fmt.Printf(formatString, v, alphabet[v])
+	}
+}
